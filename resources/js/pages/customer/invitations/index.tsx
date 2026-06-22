2a6c0250425ae5eb599101e2e2b6316e6f8009b4
santani423 <santani423@gmail.com>
2026-06-22 16:17:14 +0700

    oek

--- resources/js/pages/customer/invitations/index.tsx
+++ resources/js/pages/customer/invitations/index.tsx
@@ -260,7 +260,7 @@ function InvitationCard({ invitation }: { invitation: Invitation }) {
 export default function InvitationsIndex({ invitations }: Props) {
     return (
         <CustomerLayout breadcrumbs={breadcrumbs}>
-            <Head title="Undangan Saya" />
+            <Head title="Undangan Saya hhhhhhhhhhhhhh" />
             <div className="flex flex-col gap-6 p-6">
 
                 {/* Header */}
