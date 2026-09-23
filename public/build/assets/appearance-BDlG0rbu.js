448fe1c05afbadae684a538074ee0c7add68234e
santani423 <santani423@gmail.com>
2026-09-23 15:38:36 +0700

    o

---
+++ public/build/assets/appearance-BDlG0rbu.js
@@ -0,0 +1,16 @@
+import{u as l,j as t,L as d}from"./app-BKHY9wyE.js";import{c as r}from"./button-DLfTz-iq.js";import{c as a}from"./createLucideIcon-DqUemhdc.js";import{S as h,H as u}from"./layout-DJJ1n4kD.js";import{A as x}from"./app-layout-CulqFw-4.js";import"./separator-Zp_FUAJh.js";import"./index-BkCIrPek.js";import"./user-Bqt4xZwh.js";import"./palette-DCqcCOJ5.js";import"./app-window-BD_QRqDi.js";import"./credit-card-Cy3O1J1v.js";import"./package-Cu3YzbWy.js";import"./nav-user-D6oNnUJ3.js";import"./chevron-right-GUm1TFt7.js";import"./index-DOlcR9Mu.js";import"./index-DEqADPSt.js";import"./index-J9P-4Gul.js";import"./check-BWql4NPv.js";import"./sheet-B_NVGDAR.js";import"./x-CESLKdYP.js";import"./loader-circle-iIe5KntJ.js";import"./users-BcR10xPo.js";import"./file-text-COCpYPj_.js";import"./settings-CNVjmy3w.js";import"./book-open-DszJI1NF.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],k=a("Monitor",y);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],b=a("Moon",g);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],f=a("Sun",j);function v({className:o="",...n}){const{appearance:i,updateAppearance:s}=l(),p=[{value:"light",icon:f,label:"Light"},{value:"dark",icon:b,label:"Dark"},{value:"system",icon:k,label:"System"}];return t.jsx("div",{className:r("inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800",o),...n,children:p.map(({value:e,icon:c,label:m})=>t.jsxs("button",{onClick:()=>s(e),className:r("flex items-center rounded-md px-3.5 py-1.5 transition-colors",i===e?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"),children:[t.jsx(c,{className:"-ml-1 h-4 w-4"}),t.jsx("span",{className:"ml-1.5 text-sm",children:m})]},e))})}const A=[{title:"Appearance settings",href:"/settings/appearance"}];function Q(){return t.jsxs(x,{breadcrumbs:A,children:[t.jsx(d,{title:"Appearance settings"}),t.jsx(h,{children:t.jsxs("div",{className:"space-y-6",children:[t.jsx(u,{title:"Appearance settings",description:"Update your account's appearance settings"}),t.jsx(v,{})]})})]})}export{Q as default};
